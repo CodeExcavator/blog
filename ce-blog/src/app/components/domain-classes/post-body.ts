9564ac4481c8b81ecf70f4070fd1a69014fc768e
nifragos <nifragos@gmail.com>
2018-06-18 19:30:04 +0300

    License Fix

---
+++ ce-blog/src/app/components/domain-classes/post-body.ts
@@ -0,0 +1,4 @@
+export class PostBody{
+  textType: number;
+  postText: string;
+}
--- ce-blog/src/app/components/domain-classes/post.ts
+++ ce-blog/src/app/components/domain-classes/post.ts
@@ -1,10 +1,15 @@
+import {PostText} from "./post-body";
+
 export class Post{
   _id: string;
   title:string;
+  createdDate: Date;
+
+  postBody: PostBody[];
 
   constructor(options?: any){
     options = options || {};
     this._id = options._id || null;
-    this.title = options.title || "New category";
+    this.title = options.title || "New post";
   }
 }
